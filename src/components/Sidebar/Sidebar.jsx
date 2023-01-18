4614b61783676fdb17b7ed41f4f67980eb918bd9
Anshul Choubey <anshulchoubey25@gmail.com>
2023-01-18 14:44:20 +0530

    whole frontend ready

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react';
+import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import useStyles from './styles';
+import redLogo from '../../images/RedLogo.svg';
+import blueLogo from '../../images/BlueLogo.svg';
+import { useGetGenresQuery } from '../../services/TMDB';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+
+const categories = [
+  { label: 'Popular', value: 'popular' },
+  { label: 'Top Rated', value: 'top_rated' },
+  { label: 'Upcoming', value: 'upcoming' },
+];
+
+const Sidebar = ({ setMobileOpen }) => {
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  const theme = useTheme();
+  const classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <img
+          className={classes.image}
+          src={theme.palette.mode === 'light' ? blueLogo : redLogo}
+          alt="FilmFlix Logo"
+        />
+      </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
+              {/* <ListItemIcon>
+                <img src={blueLogo} className={classes.genreImage} height={30} alt={label} />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListSubheader>Genres</ListSubheader>
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress size="4rem" />
+          </Box>
+        ) : data.genres.map(({ name, id }) => (
+          <Link key={name} className={classes.links} to="/">
+            <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
+              {/* <ListItemIcon>
+                <img src={blueLogo} className={classes.genreImage} height={30} alt={label} />
+              </ListItemIcon> */}
+              <ListItemText primary={name} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
